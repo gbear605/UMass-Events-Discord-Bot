25de7e7ffae6f530bb6d0a5123988058551c60ef
gbear605 <garrisontaylor@gmail.com>
2019-08-29 12:04:17 -0500

    Clean up date/time handling

--- src/common/datetime.rs
+++ src/common/datetime.rs
@@ -11,8 +11,7 @@ pub fn get_datetime() -> DateTime<FixedOffset> {
     let current_time: DateTime<FixedOffset> = DateTime::from_utc(
         current_time_utc.naive_utc(),
         FixedOffset::west(4 * 60 * 60),
-        // Four hours west of the date line
-        // Four instead of five because 5am/6am is a better default than 6am/7am
+        // Four hours west of the date line, to adjust to Eastern Time in the winter
     );
 
     current_time
@@ -22,20 +21,20 @@ pub fn get_day_of_week() -> Weekday {
     get_datetime().date().weekday()
 }
 
-// Runs at 6 AM in summer or 5 AM in winter
+static HOUR_TO_RUN_AT: u32 = 5;
+
+// Runs at (HOUR_TO_RUN_AT + 1) in summer or HOUR_TO_RUN_AT in winter
 pub fn get_time_till_scheduled() -> std::time::Duration {
     let current_time = get_datetime();
-    let next_run_date = if current_time.time().hour() < 6
-        || (current_time.hour() == 6 && current_time.minute() < 5)
-    {
-        // We want to do it today (in UTC) if it is still yesterday in Eastern Time
-        current_time
-    } else {
-        current_time + chrono::Duration::days(1)
-    }
-    .date();
 
-    let next_run = next_run_date.and_hms(6, 5, 0);
+    // We want to do it today if it has yet to happen, or else tomorrow
+    let next_run_date = if current_time.time().hour() < HOUR_TO_RUN_AT {
+        current_time.date()
+    } else {
+        current_time.date() + chrono::Duration::days(1)
+    };
+
+    let next_run = next_run_date.and_hms(HOUR_TO_RUN_AT, 0, 0);
 
     (next_run - current_time).to_std().unwrap()
 }
